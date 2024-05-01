e7bd7babe4f48904ff9383f473ac60901cea7de3
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2024-05-01 22:26:08 +0530

    Update vpc.tf

--- CONTINEOUS-DEPLOYMENT/vpc.tf
+++ CONTINEOUS-DEPLOYMENT/vpc.tf
@@ -1,23 +1,34 @@
+
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.7.0"
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-south-1a", "ap-south-1b" ]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-
-  enable_vpn_gateway = true
-map_public_ip_on_launch = true
-
-enable_nat_gateway = true
-single_nat_gateway = true
-one_nat_gateway_per_az = false
+  name                 = "eks-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    "kubernetes.io/cluster/my-cluster" = "shared"
   }
+
+ public_subnet_tags = {
+  "kubernetes.io/cluster/my-cluster" = "shared"
+  "kubernetes.io/role/elb"           = "1"
+  "subnet-type"                       = "public"
 }
+
+private_subnet_tags = {
+  "kubernetes.io/cluster/my-cluster" = "shared"
+  "kubernetes.io/role/internal-elb"  = "1"
+  "subnet-type"                      = "private"
+}
+
+}
+
+data "aws_availability_zones" "available" {}
